810ecb299f162d00cb0da590d004deecc65080a7
Kilyan Halimi <halimikilyan@gmail.com>
2024-06-25 16:59:46 +0200

    Ajout des controller/route de base des Models

---
+++ SERVER/routes/categoryController.js
@@ -0,0 +1,88 @@
+const { Router } = require("express");
+const Category = require("../models/category");
+const checkAuth = require("../middlewares/checkAuth");
+const router = new Router();
+
+router.get("/", checkAuth, async (req, res, next) => {
+  const categories = await Category.findAll({
+    where: req.query,
+  });
+  res.json(categories);
+});
+
+router.post("/", async (req, res, next) => {
+  try {
+    const category = await Category.create(req.body);
+    res.status(201).json(category);
+  } catch (e) {
+    next(e);
+  }
+});
+
+router.get("/:id", async (req, res, next) => {
+  try {
+    const category = await Category.findByPk(parseInt(req.params.id));
+    if (category) {
+      res.json(category);
+    } else {
+      res.sendStatus(404);
+    }
+  } catch (e) {
+    next(e);
+  }
+});
+
+router.patch("/:id", async (req, res, next) => {
+  try {
+    const [nbUpdated, categories] = await Category.update(req.body, {
+      where: {
+        id: parseInt(req.params.id),
+      },
+      returning: true,
+      individualHooks: true,
+    });
+    if (categories[0]) {
+      res.json(categories[0]);
+    } else {
+      res.sendStatus(404);
+    }
+  } catch (e) {
+    next(e);
+  }
+});
+
+router.delete("/:id", async (req, res, next) => {
+  try {
+    const nbDeleted = await Category.destroy({
+      where: {
+        id: parseInt(req.params.id),
+      },
+    });
+    if (nbDeleted === 1) {
+      res.sendStatus(204);
+    } else {
+      res.sendStatus(404);
+    }
+  } catch (e) {
+    next(e);
+  }
+});
+
+router.put("/:id", async (req, res, next) => {
+  try {
+    const nbDeleted = await Category.destroy({
+      where: {
+        id: parseInt(req.params.id),
+      },
+    });
+    const category = await Category.create({
+      ...req.body,
+      id: parseInt(req.params.id),
+    });
+    res.status(nbDeleted ? 200 : 201).json(category);
+  } catch (e) {
+    next(e);
+  }
+});
+
+module.exports = router;
