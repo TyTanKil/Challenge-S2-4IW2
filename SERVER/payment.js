302559ddc97d62c1f151705138812c9bff2883ee
HugoAndre15 <hugo.andre1501@gmail.com>
2024-07-24 11:21:11 +0200

    fix

--- SERVER/payment.js
+++ SERVER/payment.js
@@ -3,7 +3,7 @@ const express = require("express");
 const Stripe = require("stripe");
 const bodyParser = require("body-parser");
 const cors = require("cors");
-import apiClient from './assets/js/apiClient';
+
 
 const stripe = new Stripe(
   "sk_test_51Pb4ZxRplArNYE0A1twbt7PDqr1ADz0oSLM0vyOIEwmvCmNIkKUxddGRoBLwea805YkInglHBk4NZs4RZg6fNqJg00vZAYL70g",
@@ -43,8 +43,8 @@ router.post("/create-checkout-session", async (req, res) => {
       payment_method_types: ["card"],
       line_items: lineItems,
       mode: "payment",
-      success_url: `${APiClient}/success?session_id={CHECKOUT_SESSION_ID}`,
-      cancel_url: `${APiClient}/cancel`,
+      success_url: `${import.meta.env.VITE_FRONT_ENDPOINT}/success?session_id={CHECKOUT_SESSION_ID}`,
+      cancel_url: `${import.meta.env.VITE_FRONT_ENDPOINT}/cancel`,
     });
 
     res.json({ id: session.id });
